4a83815c60aa8b4bc909f12f1d22f452fc41fd4d
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2023-01-02 23:16:31 +0900

    CRUDの実装

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -2,14 +2,44 @@ import React from 'react';
 import './style.css';
 
 export const App = () => {
+  const [todoText, setTodoText] = React.useState('');
   const [imcompleteTodos, setImcompleteTodos] = React.useState(['ああああああああああ', 'iiiiiiiiiii'])
   const [completeTodos, setCompleteTodos] = React.useState(['うううううううううう', 'eeeeeeeeeee'])
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  const onClickAdd = () => {
+    if (todoText === '') return;
+    const newTodos = [...imcompleteTodos, todoText];
+    setImcompleteTodos(newTodos);
+    setTodoText('');
+  };
+  const onClickDelete = (index) => {
+    const newTodos = [...imcompleteTodos];
+    newTodos.splice(index, 1);
+    setImcompleteTodos(newTodos);
+  };
+  const onClickComplete = (index) => {
+    const newImcompleteTodos = [...imcompleteTodos];
+    newImcompleteTodos.splice(index, 1);
+    setImcompleteTodos(newImcompleteTodos);
+
+    const newCompleteTodos = [...completeTodos, imcompleteTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+  }
+
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+    setCompleteTodos(newCompleteTodos);
+
+    const newImcompleteTodos = [...imcompleteTodos, completeTodos[index]];
+    setImcompleteTodos(newImcompleteTodos);
+  }
 
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力"/>
-        <button>追加</button>
+        <input placeholder="TODOを入力" value={todoText} onChange={ onChangeTodoText } />
+        <button onClick={ onClickAdd }>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
@@ -18,8 +48,8 @@ export const App = () => {
             return (
               <div className="list-row" key={index}>
                 <li>{todo}</li>
-                <button>完了</button>
-                <button>削除</button>
+                <button onClick={ () => onClickComplete(index) }>完了</button>
+                <button onClick={ () => onClickDelete(index) }>削除</button>
               </div>
             );
           })}
@@ -32,7 +62,7 @@ export const App = () => {
             return (
               <div className="list-row" key={index}>
                 <li>{todo}</li>
-                <button>戻す</button>
+                <button onClick={ () => onClickBack(index) }>戻す</button>
               </div>
             );
           })}
