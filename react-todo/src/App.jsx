41a6ae5cf7fff34b4de9b5cfb1494669a84bb5ba
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2023-01-02 16:24:54 +0900

    mockの作成

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,9 +1,45 @@
-function App() {
+import React from 'react';
+import './style.css';
+
+export const App = () => {
+  const [imcompleteTodos, setImcompleteTodos] = React.useState(['ああああああああああ', 'iiiiiiiiiii'])
+  const [completeTodos, setCompleteTodos] = React.useState(['うううううううううう', 'eeeeeeeeeee'])
+
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力"/>
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {imcompleteTodos.map((todo, index) => {
+            return (
+              <div className="list-row" key={index}>
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          {completeTodos.map((todo, index) => {
+            return (
+              <div className="list-row" key={index}>
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
 }
 
-export default App;
+export default App; 
